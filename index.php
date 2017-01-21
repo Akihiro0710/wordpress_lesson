f6c50db8d62576647595b85ff0d83ac01964fffe
akihiro0710 <acedia2074@gmail.com>
2017-01-22 04:35:50 +0900

    titleを表示

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<title><?php bloginfo( 'neme' ); wp_title(); ?></title>
 	<link rel="stylesheet" href="<? echo get_stylesheet_uri();	// style.cssの読み込み ?>">
 </head>
 <body <?php body_class();	// ページの区別用class ?>>
