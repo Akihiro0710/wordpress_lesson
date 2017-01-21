9408d5128acc45b4a8dc2985c8e858568dcc77e0
akihiro0710 <acedia2074@gmail.com>
2017-01-22 03:45:32 +0900

    記事を区別できるようにする

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 	<meta charset="UTF-8">
 	<title></title>
 </head>
-<body>
+<body <?php body_class();	// ページの区別用class ?>>
 <?php
 if ( have_posts() ):	// 記事が存在するか
 	while ( have_posts() ):	// 記事が存在する間ループ
 		the_post();	// 記事を取得
 ?>
-<article>
-	<h1><?php the_title();	// 記事のタイトル?></h1>
-	<?php the_content();	// 記事のコンテンツ?>
+<article <?php post_class();	// 記事の区別用class ?>>
+	<h1><?php the_title();	// 記事のタイトル ?></h1>
+	<?php the_content();	// 記事のコンテンツ ?>
 </article>
 <?php
 	endwhile;
