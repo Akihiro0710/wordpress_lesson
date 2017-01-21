f26fd005a868eb19902ec0c6dd8645d22752929f
akihiro0710 <acedia2074@gmail.com>
2017-01-22 03:38:51 +0900

    記事をループで取得

--- index.php
+++ index.php
@@ -5,7 +5,16 @@
   <title></title>
 </head>
 <body>
-<?php the_title(); ?>
-<?php the_content(); ?>
+<?php
+if ( have_posts() ):	// 記事が存在するか
+	while ( have_posts() ):	// 記事が存在する間ループ
+		the_post();	// 記事を取得
+?>
+<?php the_title(); // 記事のタイトル?>
+<?php the_content(); // 記事のコンテンツ?>
+<?php
+	endwhile;
+endif;
+?>
 </body>
 </html>
