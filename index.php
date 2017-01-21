3f34c154ce27fe634e302a79ec491a9f3b0f21f8
akihiro0710 <acedia2074@gmail.com>
2017-01-22 03:40:40 +0900

    記事をマークアップ

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <title></title>
+	<meta charset="UTF-8">
+	<title></title>
 </head>
 <body>
 <?php
@@ -10,8 +10,10 @@ if ( have_posts() ):	// 記事が存在するか
 	while ( have_posts() ):	// 記事が存在する間ループ
 		the_post();	// 記事を取得
 ?>
-<?php the_title(); // 記事のタイトル?>
-<?php the_content(); // 記事のコンテンツ?>
+<article>
+	<h1><?php the_title();	// 記事のタイトル?></h1>
+	<?php the_content();	// 記事のコンテンツ?>
+</article>
 <?php
 	endwhile;
 endif;
